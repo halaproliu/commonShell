5f2d5f56d84adb5d5e6a0963a2e98f41953d6739
luffy <luffybryant@gmail.com>
2019-11-10 20:31:33 +0800

    update

--- fn.sh
+++ fn.sh
@@ -26,4 +26,11 @@ toFirstLetterUpper2() {
 }
 
 
-toFirstLetterUpper1 eco
+# ${var#*/}  最小限度从前面截取字符串，分隔符/
+# ${var##*/}  最大限度从前面截取字符串，分隔符/
+# ${var%/*} 最小限度从后面截取字符串，分隔符/
+# ${var%%/*} 最大限度从后面截取字符串，分隔符/
+# ${var:start:len} 截取字符串，从start位置开始（最小为0），len是截取长度
+# ${var:start}
+# ${var:0-start:len} 从右边开始算，第start个字符开始，len是截取长度
+# ${var:0-start}
