b4e0f28376fcf3b589665dcf4c6492c272e15d0a
luffy <luffybryant@gmail.com>
2019-11-10 19:54:42 +0800

    update

--- fn.sh
+++ fn.sh
@@ -9,5 +9,21 @@ toFirstLetterUpper(){
   result=$firstLetter$otherLetter
 }
 
-toFirstLetterUpper 'eco'
-echo $result
+toFirstLetterUpper1(){
+  str=$1
+  firstLetter=${str:0:1}
+  otherLetter=${str:1}
+  firstLetter=$(echo $firstLetter | tr '[:lower:]' '[:upper:]')
+  result=$firstLetter$otherLetter
+}
+
+# 首字母转换为大写
+toFirstLetterUpper2() {
+  str=$1
+  firstLetter=`echo ${str:0:1} | awk '{print toupper($0)}'`
+  otherLetter=${str:1}
+  result=$firstLetter$otherLetter
+}
+
+
+toFirstLetterUpper1 eco
